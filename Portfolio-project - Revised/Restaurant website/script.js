2716528c61bb3fcfba82f6bacd65a3a72015c59e
Brianmill <153027822+Brianmill@users.noreply.github.com>
2023-12-05 17:30:54 -0500

    Delete Portfolio-project - Revised directory

--- Portfolio-project - Revised/Restaurant website/script.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    parallax();
-})
-
-function parallax() {
-    var wScroll = $(window).scrollTop();
-    $('#parallax--mb').css('background-position','center '+(wScroll*0.4)+'px');
-    console.log(wScroll);
-}
--- Portfolio-project - Revised/Review-project/myscript.js
+++
@@ -1,7 +0,0 @@
-function openNew() {
-    window.open("Reviews.html");
-  }
-
-
-
-  
